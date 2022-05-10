34ec3cdc82bdf9965dba5ba65b4bc72e0a17130b
apriljade <seonggyu@kakao.com>
2022-05-10 12:02:21 +0900

    Implement hexdump for stdin

--- includes/hex_dump_tools.h
+++ includes/hex_dump_tools.h
@@ -3,17 +3,7 @@
 #include "../includes/general.h"
 #include "../includes/optvec.h"
 
-void printLine(char *data, int total_size, int size, int flag);
-void PrintIndexInHex(int size, int flag, int last);
-void PrintDataInHex(uchar *data, int size, int flag);
-void PrintDataInAscii(uchar *data, int size);
-void PrintRemainData(char *data, int size, int flag);
-void charcat(char *dest, char c);
-void PrintSpaces(int len, int flag);
-void PrintIntinHex(int number, const char *hex);
-
 void PrintIndex(int size);
-
 void PrintLine(optvec* options, uchar* data, int size, int totalSize);
 
 #endif
